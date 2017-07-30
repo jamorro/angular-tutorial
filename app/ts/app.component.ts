59899eed3e5c6a12dbc9c790f974b8e65fb5b3cc
Joakim Järvinen (Consultant) <jarjoa@intra.ip-only.net>
2017-07-30 18:18:15 +0200

    started angular
    halvfärdig tutorial

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {PlaylistComponent} from "./playlist.component";
+import {Video} from "./video";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+    constructor(){
+        this.videos = [
+            new Video(1,"Installing django", "qgGIqRFvFFk", "How to isntall django"),
+            new Video(2,"Surviving the Wilderness", "Fgwy-UdtyLs", "Hiking woop")
+
+        ]
+    }
+}
